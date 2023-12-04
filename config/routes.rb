82f0171cadd93f14ea6629dec230c5ad1d1a7a9e
triciasykes <tlsykes98@gmail.com>
2023-12-04 13:59:53 -0800

    added edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get '/books/new' => 'book#new', as: 'new_book'
   get '/books/:id' => 'book#show', as: 'book'
   post '/books' => 'book#create'
+  get 'books/:id/edit' => 'book#edit', as: 'edit_book'
+  patch 'books/:id' => 'book#update'
   delete '/books/:id' => 'book#destroy', as: 'delete_book'
 
   # Defines the root path route ("/")
