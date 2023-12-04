2197473af5a9c579dc8d1432baf7d5f3c28081da
triciasykes <tlsykes98@gmail.com>
2023-12-04 12:04:08 -0800

    added new create and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # verb 'url' hashrocket 'controller#method'
 
   get '/books' => 'book#index', as: 'books'
+  get '/books/new' => 'book#new', as: 'new_book'
   get '/books/:id' => 'book#show', as: 'book'
+  post '/books' => 'book#create'
+  delete '/books/:id' => 'book#destroy', as: 'delete_book'
 
   # Defines the root path route ("/")
    root "book#index"
